a06b5a528a9b926be219ca1268249e5256638f6d
JBoas <jboas@sjnma.org>
2017-09-18 13:33:02 -0500

    Primative Variables

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+name=input("Please enter your name ")
+teacher = ("Mr.Gold ")
+
+print ("Nice to meet you, ")
+print (name)
+
+print (name + " is a great cadet "+ teacher.upper() +  " tends to agree")
+
+
---
+++ math.py
@@ -0,0 +1,19 @@
+a=20+35
+print(a)
+
+y = 3*a + 40
+
+print(y)
+i = 50
+print(i)
+
+i = i + 2
+i = i + 10
+
+print(i)
+
+radius = 25
+diameter = 2*radius
+pi = 3.1415268987
+circumference = pi * radius*2
+print(
