45f2e9e987deb4ad16bfc8f49810a00952f0b6cd
JBoas <jboas@sjnma.org>
2017-09-19 13:39:49 -0500

    classwork

---
+++ words.py
@@ -0,0 +1,17 @@
+students=["Engel","Slosar","Boas","Spice" ]
+students.append(input("enter student"))
+print(students)
+students.append(input("enter student"))
+print(students)
+
+
+
+gpas = [4,5.4,5.4,4.5,4.7]
+print (gpas[3])
+
+message = "The Quick Brown Fox Jumped Over The Fence"
+print(message)
+words = message.split()
+print(words)
+lines = "-".join(words)
+print(lines)
