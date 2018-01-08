cc9ce6fcdac94aa02d5168b45d5a10ab671896ee
JBoas <jboas@sjnma.org>
2018-01-08 15:39:30 -0600

    .

---
+++ dodearea.py
@@ -0,0 +1,6 @@
+a = input("Enter length of edge of dodecahedron")
+
+
+a = int(a)
+Area = (((((((a**2)*5)**.5)*10)+25)**.5)*3)
+print(Area)
---
+++ timeconv.py
@@ -0,0 +1,7 @@
+seconds = input("Enter Time in Seconds")
+
+seconds = float(seconds)
+minutes = (seconds)/60
+hours = (minutes)/60
+
+print(str(seconds) + " Seconds,", str(minutes) + " Minutes,", "and", str(hours) + " Hours")
